0347461517b72bfd1b1f80e30dae055983947ac1
Ahmed Salman <haameed.salman@gmail.com>
2026-07-05 13:49:12 +0100

    fix: set ALLOWED_HOSTS to wildcard for Railway

--- campusserve/settings/production.py
+++ campusserve/settings/production.py
@@ -2,19 +2,19 @@ from .base import *
 import os
 
 DEBUG = False
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     "default": {
         "ENGINE":   "django.db.backends.postgresql",
-        "NAME":     os.environ.get("PGDATABASE", os.environ.get("POSTGRES_DB", "railway")),
-        "USER":     os.environ.get("PGUSER", os.environ.get("POSTGRES_USER", "postgres")),
-        "PASSWORD": os.environ.get("PGPASSWORD", os.environ.get("POSTGRES_PASSWORD", "")),
+        "NAME":     os.environ.get("PGDATABASE", "railway"),
+        "USER":     os.environ.get("PGUSER", "postgres"),
+        "PASSWORD": os.environ.get("PGPASSWORD", ""),
         "HOST":     os.environ.get("PGHOST", "localhost"),
         "PORT":     os.environ.get("PGPORT", "5432"),
     }
 }
 
 SECURE_SSL_REDIRECT = False
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = False
